f13f86d1fbbc2c72fc95396ed6b6c1796d4879b7
Rayyan Ali Akbar <alphatrooper335@gmail.com>
2025-01-23 22:26:17 +0530

    updated the ui with streamlit to accept multiple pdfs and exract text from them as well as make them into chunks

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(separator="\n",
+    chunk_size=1000,
+    chunk_overlap=200,
+    length_function=len
+    )
+
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+
+
 
 def main():
     load_dotenv()
@@ -10,9 +34,16 @@ def main():
 
     with st.sidebar:
         st.subheader("Your Documents")
-        st.file_uploader("Upload you PDF files here and click the submit button")
-        st.button("Submit")
+        pdf_docs = st.file_uploader(
+            "Upload you PDF files here and click the submit button",accept_multiple_files=True)
+        if st.button("Submit"):
+            with st.spinner("Processing"):
+                #get the pdf text 
+                raw_text = get_pdf_text(pdf_docs)
 
+                #get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                #create vector store
 
 if __name__=='__main__':
     main()
