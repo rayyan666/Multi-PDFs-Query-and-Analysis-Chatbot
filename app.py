5b155fb71d5a8045db52c97d50f9a386f66e51f0
Rayyan Ali Akbar <alphatrooper335@gmail.com>
2025-01-25 16:07:12 +0530

    completed the project and fixed some bugs

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import HuggingFaceHub
+from htmlpage import css,bot_template,user_template
+import os
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -25,17 +31,57 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-    embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-mpnet-base-v2")
+    embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    load_dotenv()
+    hf_api = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+    if hf_api is None:
+       raise ValueError("API_KEY environment variable not set")
+    
+    #llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
+    llm = HuggingFaceHub(repo_id="google/flan-t5-base", model_kwargs={"temperature": 0.7, "max_length": 512}, huggingfacehub_api_token=hf_api)
+    memory = ConversationBufferMemory(memory_key= 'chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory = memory
+
+    )
+    return conversation_chain
+
+def handle_user_input(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
 
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDF files", page_icon=":books:")
 
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+
     st.header("Chat with multiple PDF files :books:")
-    st.text_input("Ask a question about the relevant document:")
+    user_question = st.text_input("Ask a question about the relevant document:")
+    if user_question:
+            handle_user_input(user_question)
 
     with st.sidebar:
         st.subheader("Your Documents")
@@ -52,5 +98,7 @@ def main():
                 #create vector store
                 vectorstore = get_vectorstore(text_chunks)
 
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+ 
 if __name__=='__main__':
     main()
